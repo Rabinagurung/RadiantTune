f8698cfb821cd279924e387c72a8f8cd1de40a12
Rabinagurung <rabinagurung347@gmail.com>
2024-05-16 19:32:28 -0400

    Testing changes

---
+++ RadiantTune/Viewcontrollers/Setting/ViewController/RTSettingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RTSettingViewController.swift
+//  RadiantTune
+//
+//  Created by 杜乐乐 on 2024-05-11.
+//
+
+import UIKit
+
+class RTSettingViewController: RTBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
