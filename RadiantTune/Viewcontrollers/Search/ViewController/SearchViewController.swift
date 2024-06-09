d2ab1b6e290897e54b5cc1f68363bd7b1fc96fbb
Elvis Nguyen <elvisnguyen1@outlook.com>
2024-06-09 07:30:24 -0400

    Warning: Locked Table Cell Constraints, need help

--- RadiantTune/Viewcontrollers/Search/ViewController/SearchViewController.swift
+++ RadiantTune/Viewcontrollers/Search/ViewController/SearchViewController.swift
@@ -8,14 +8,28 @@
 import Foundation
 import UIKit
 import Moya
+import Kingfisher
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchTableView: UITableView!
+    
     var searchString: String?
     var stations = [APIStation]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+        
+        searchTableView.rowHeight = UITableView.automaticDimension
+        
+        searchTableView.estimatedRowHeight = 150
+        searchTableView.register(UINib(nibName: "StationTableCell", bundle: nil), forCellReuseIdentifier: "StationTableCell")
+        
         
         if let searchString = searchString {
             searchBar.text = searchString
@@ -23,30 +37,64 @@ class SearchViewController: UIViewController {
             searchbyname(searchString: searchString)
         }
         
-        func searchbyname(searchString:String)
-        {
-            let moya = MoyaProvider<RadioAPI>()
-            /*
-            moya.request(RadioAPI.searchStations(codec: .none, order: .none, reverse: .none, limit: 1)) */
-            moya.request(RadioAPI.searchbyname(searchTerm: "The beat") ) { result in
-                switch result {
-                    case let .success(moyaResponse):
-                        do {
-                            //try moyaResponse.filterSuccessfulStatusCodes()
-                            //let data = try moyaResponse.mapJSON()
-                            
-                            let decoder = JSONDecoder()
-                            let stations = try decoder.decode([Station].self, from: moyaResponse.data)
-                            debugPrint(stations)
-                            //debugPrint(data)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = searchTableView.dequeueReusableCell(withIdentifier: "StationTableCell", for: indexPath) as! StationTableCell
+        let station = stations[indexPath.row]
+        
+        cell.lblStationName.text = station.name
+        
+        if let faviconURL = station.favicon {
+            let url = URL(string: faviconURL)
+            cell.imgStationFavicon.kf.setImage(with: url, placeholder: UIImage(named: "default_station.jpg"))
+        }
+        else {
+            cell.imgStationFavicon.image = UIImage(named: "default_station.jpg")
+        }
+            
+        return cell
+    }
+    
+    func searchbyname(searchString:String)
+    {
+        let moya = MoyaProvider<RadioAPI>()
+        /*
+        moya.request(RadioAPI.searchStations(codec: .none, order: .none, reverse: .none, limit: 1)) */
+        moya.request(RadioAPI.searchbyname(searchTerm: "The beat") ) { result in
+            switch result {
+                case let .success(moyaResponse):
+                    do {
+                        //try moyaResponse.filterSuccessfulStatusCodes()
+                        //let data = try moyaResponse.mapJSON()
+                        
+                        let decoder = JSONDecoder()
+                        self.stations = try decoder.decode([APIStation].self, from: moyaResponse.data)
+                        DispatchQueue.main.async {
+                            self.searchTableView.reloadData()
                         }
-                        catch {
-                            print(error)
-                        }
-                    case let .failure(error):
-                        print(error.localizedDescription)
+                        /*
+                        for station in stations {
+                            debugPrint("Name: \(station.name!), Image: \(station.favicon!), URL:\(station.url!)")
+                        }*/
+                        //debugPrint(stations)
+                        //debugPrint(data)
                     }
-            }
+                    catch {
+                        print(error)
+                    }
+                case let .failure(error):
+                    print(error.localizedDescription)
+                }
         }
     }
+
 }
---
+++ RadiantTune/Viewcontrollers/Search/ViewController/StationTableCell.swift
@@ -0,0 +1,15 @@
+//
+//  StationTableCell.swift
+//  RadiantTune
+//
+//  Created by Elvis Nguyen on 2024-06-09.
+//
+
+import Foundation
+import UIKit
+
+class StationTableCell: UITableViewCell {
+    
+    @IBOutlet weak var lblStationName: UILabel!
+    @IBOutlet weak var imgStationFavicon: UIImageView!
+}
