965a0deea4137534d93f68e1a57f2945e63fc99b
Elvis Nguyen <elvisnguyen1@outlook.com>
2024-07-21 15:56:01 -0400

    Closes #25, closes #28 ; no new code

--- RadiantTune/Others/AppDelegate.swift
+++ RadiantTune/Others/AppDelegate.swift
@@ -11,7 +11,7 @@ import AVKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+///
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         DispatchQueue.global(qos: .userInitiated).async {
